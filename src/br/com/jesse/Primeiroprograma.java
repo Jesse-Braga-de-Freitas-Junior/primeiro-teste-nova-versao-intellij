5c52e04803e734ae84b3c860a80463f32c04317d
Jesse Junior <jbfjunior2009@gmail.com>
2021-12-12 16:14:17 -0300

    first commit

---
+++ src/br/com/jesse/Primeiroprograma.java
@@ -0,0 +1,16 @@
+package br.com.jesse;
+
+import br.com.jesse.model.Gato;
+
+public class Primeiroprograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 2;
+        int b = 3;
+        System.out.println(" Hello world!! " + (a+b));*/
+    }
+
+}
